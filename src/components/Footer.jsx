676f801613a240bb452ebf9e661dc4b5d8fb0f9b
Rizwan <imrizwan164@gmail.com>
2026-08-19 17:11:35 +0500

    feat: Added Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer(){
     return (
       <div className="text-center bg-[#161311] border-t border-[#2B3533] p-3">
         <p className="text-[#EDE6D6]/40 text-[11px] md:text-[13px]">© 2026 Rizwan Ali</p>
-        <p className="text-[#EDE6D6]/40 text-[11px] mt-2 md:text-[13px]">This product uses the TMDB API but is not endorsed or certified by TMDB.</p>
+        <p className="text-[#EDE6D6]/40 text-[11px] mt-2 md:text-[13px]">This product uses the OMDB API but is not endorsed or certified by OMDB.</p>
       </div>
     )
 }
